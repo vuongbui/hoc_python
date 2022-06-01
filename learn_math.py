03e3c14fbd6223c5428dd1f584ba77418bfaf78b
vuongbui <vuong@foretheta.com>
2022-06-01 19:32:06 +0700

    first commit

---
+++ learn_math.py
@@ -0,0 +1,20 @@
+# This is an addtion calculation
+print(12568 + 225588)
+
+# Subtraction
+print(343 - 23)
+
+# Multiplication
+print(4 * 5)
+
+# Division
+print(10 / 3)
+
+# Exponent
+print(8**5)
+
+# Floor division
+print(10 // 3)  # kq = 3
+
+# modulo
+print(10 % 3)   # kq bang 1
